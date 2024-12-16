8b1e9e72186a6f7cbbcc8f42850a0fec46ce5f0e
antonio <antonio>
2024-12-16 22:55:17 +0000

    ya no se

--- scripts/deploy_moodle_frontend.sh
+++ scripts/deploy_moodle_frontend.sh
@@ -14,6 +14,9 @@ rm -rf /tmp/v4.3.1.zip*
 rm -rf /var/www/html/*
 rm -rf /var/moodledata/*
 
+# Creo carpeta moodledata (intento)
+sudo mkdir -p /var/moodledata
+
 # Cambiamos propietario y grupo
 sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 755 /var/www/html
@@ -30,27 +33,31 @@ wget https://github.com/moodle/moodle/archive/refs/tags/v4.3.1.zip -P /tmp
 # Descomprimo el zip y lo muevo
 unzip /tmp/v4.3.1.zip -d /tmp
 mv /tmp/moodle-4.3.1/* $MOODLE_DIRECTORY
+
 # Eliminamos el zip porque ya lo hemos descomprimido
 # rm -rf $MOODLE_DIRECTORY/v4.3.1.zip (lo comento por si acaso me está generando error pero lo dudo bastante)
 
 # Instalación de Moodle con CLI
-sudo -u www-data php $MOODLE_DIRECTORY/admin/cli/install.php \
-  --lang=MOODLE_LANG \
-  --wwwroot=$MOODLE_WWWROOT \
-  --dataroot=$MOODLE_DATAROOT \
-  --dbtype=$MOODLE_DB_TYPE \
-  --dbhost=$MOODLE_DB_HOST \
-  --dbname=$MOODLE_DB_NAME \
-  --dbuser=$MOODLE_DB_USER \
-  --dbpass=$MOODLE_DB_PASS \
-  --fullname="$MOODLE_FULLNAME" \
-  --shortname="$MOODLE_SHORTNAME" \
-  --summary="$MOODLE_SUMMARY" \
-  --adminuser=$MOODLE_ADMIN_USER \
-  --adminpass=$MOODLE_ADMIN_PASS \
-  --adminemail=$MOODLE_ADMIN_EMAIL \
-  --non-interactive \
-  --agree-license
+sudo -u www-data php /var/www/html/admin/cli/install.php \
+    --lang=$MOODLE_LANG \
+    --wwwroot=$MOODLE_WWWROOT \
+    --dataroot=$MOODLE_DATAROOT \
+    --dbtype=$MOODLE_DB_TYPE \
+    --dbhost=$MOODLE_DB_HOST \
+    --dbname=$MOODLE_DB_NAME \
+    --dbuser=$MOODLE_DB_USER \
+    --dbpass=$MOODLE_DB_PASS \
+    --fullname="$MOODLE_FULLNAME" \
+    --shortname="$MOODLE_SHORTNAME" \
+    --summary="$MOODLE_SUMMARY" \
+    --adminuser=$MOODLE_ADMIN_USER \
+    --adminpass=$MOODLE_ADMIN_PASS \
+    --adminemail=$MOODLE_ADMIN_EMAIL \
+    --non-interactive \
+    --agree-license
+
+# Modulo para reescribir URLs en apache (lo pongo por descartar algo después de tantos intentos)
+a2enmod rewrite
 
 # Configuramos proxy inverso con sed
 sed -i "/\$CFG->admin/a \$CFG->reverseproxy=1;\n\$CFG->sslproxy=1;" /var/www/html/config.php
